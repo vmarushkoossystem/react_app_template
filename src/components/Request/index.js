64c959f8ee1ca9819010b0793b0b2d6a7606af37
Vitalii Marushko <vi.marushko@gmail.com>
2019-12-06 23:33:59 +0200

    Added axios and test request

--- src/components/Request/index.js
+++ src/components/Request/index.js
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 
 const Request = () => {
+  let [itemId, setItemId] = useState(1);
+  let [content, setContent] = useState('No content');
+
+  const makeRequest = async () => {
+    let content = await axios(`https://jsonplaceholder.typicode.com/todos/${itemId}`);
+    setContent(JSON.stringify(content.data, null, 2));
+    setItemId(itemId + 1);
+  };
+
   return (
-    <div>
-      Request page
-    </div>
+    <>
+      <button type="button" onClick={makeRequest}>Make request</button>
+      <pre>{content}</pre>
+    </>
   );
 };
 
